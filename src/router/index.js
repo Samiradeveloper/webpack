4668255a761ab1f4bcfd91fd0b67e12cf86953ad
Samiradeveloper <s.ghadiri1994@gmail.com>
2022-06-14 07:22:59 -0700

    remove googlefont add fontawesome fix #1

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import font from 'font-awesome/css/font-awesome.min.css'
+
 
 Vue.use(Router)
 
